51b768826a358aaca6a19098f38e557a110e81d1
Jay Gao <gaoljie@gmail.com>
2019-05-20 00:08:47 +0800

    jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ["js", "json", "vue"],
-  transform: {
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-    ".*\\.(vue)$": "vue-jest"
-  },
-  snapshotSerializers: ["jest-serializer-vue"]
-};
